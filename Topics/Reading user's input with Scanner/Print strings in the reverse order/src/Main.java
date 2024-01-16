1cb2bdc93f0768b25c38a66ff1f07f515e1fe577
Duc Huy Ly <duc-huy.ly@etu.u-bordeaux.fr>
2024-01-16 22:44:05 +0100

    Initial commit

---
+++ Topics/Reading user's input with Scanner/Print strings in the reverse order/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        String string1 = scanner.next();
+        String string2 = scanner.next();
+        String string3 = scanner.next();
+        System.out.println(string3 + "\n"+ string2 + "\n"+string1);
+    }
+}
